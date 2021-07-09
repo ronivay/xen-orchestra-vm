82ad1f6938b5c933fccc372287e381dff243019e
ronivay <roni@vayrynen.info>
2021-07-09 16:56:36 +0300

    fix mistake in rootlogin ssh config

--- roles/vm-configure/files/ssh-setup.sh
+++ roles/vm-configure/files/ssh-setup.sh
@@ -7,6 +7,6 @@ rm -f /etc/ssh/ssh_host_*_key /etc/ssh/ssh_host_*key.pub
 dpkg-reconfigure openssh-server
 
 # disable rootlogin through SSH
-sed -i 's/PermitRootLogin without-password/#PermitRootLogin without-password/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin without-password/PermitRootLogin no/' /etc/ssh/sshd_config
 
 systemctl restart sshd
